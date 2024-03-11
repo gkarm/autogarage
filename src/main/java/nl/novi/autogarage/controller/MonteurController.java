f4b10a588995057e088fb0e4be031a131b121654
gkarm <jlapalo@yahoo.com>
2024-03-11 14:59:12 +0100

    new commit

--- src/main/java/nl/novi/autogarage/controller/MonteurController.java
+++ src/main/java/nl/novi/autogarage/controller/MonteurController.java
@@ -4,10 +4,11 @@ import nl.novi.autogarage.model.Monteur;
 import nl.novi.autogarage.repository.MonteurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,25 @@ public class MonteurController {
     @GetMapping
     public ResponseEntity<List<Monteur>> getAllMonteurs() {
         return ResponseEntity.ok(monteurRepository.findAll());
+
+    }
+
+    @GetMapping("/after")
+    public ResponseEntity<List<Monteur>> getMonteursAfter(@RequestParam LocalDate date) {
+        return ResponseEntity.ok(monteurRepository.findByDobAfter(date));
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<Monteur> createMonteur(@RequestBody Monteur monteur) {
+        monteurRepository.save(monteur);
+        URI uri = URI.create(
+                ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/" + monteur.getId()).toUriString());
+
+        return ResponseEntity.created(uri).body(monteur);
 
     }
 
--- src/main/java/nl/novi/autogarage/repository/MonteurRepository.java
+++ src/main/java/nl/novi/autogarage/repository/MonteurRepository.java
@@ -3,5 +3,9 @@ package nl.novi.autogarage.repository;
 import nl.novi.autogarage.model.Monteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MonteurRepository extends JpaRepository<Monteur,Long> {
+    public List<Monteur> findByDobAfter(LocalDate date);
 }
